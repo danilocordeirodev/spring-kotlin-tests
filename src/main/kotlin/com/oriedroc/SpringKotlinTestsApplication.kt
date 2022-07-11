df8c044b1c0d1b5a495cce24a6faafc5a1cfdf50
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2022-07-10 23:12:46 -0300

    initial commit

---
+++ src/main/kotlin/com/oriedroc/SpringKotlinTestsApplication.kt
@@ -0,0 +1,11 @@
+package com.oriedroc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinTestsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinTestsApplication>(*args)
+}
---
+++ src/test/kotlin/com/oriedroc/SpringKotlinTestsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oriedroc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinTestsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
